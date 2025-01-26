75ddf17883955715462b6a5fc430d3b1d5d2db65
DanielKGM <atbreezy4@gmail.com>
2025-01-26 15:49:12 -0300

    estrutura inicial

---
+++ src/app/components/title-toolbar/title-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-toolbar',
+  imports: [],
+  templateUrl: './title-toolbar.component.html',
+  styleUrl: './title-toolbar.component.scss'
+})
+export class TitleToolbarComponent {
+
+}
