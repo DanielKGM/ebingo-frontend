25226625a3ad238c2e59ff2d7390c8fc06ac07bd
DanielKGM <atbreezy4@gmail.com>
2025-02-10 22:28:01 -0300

    chore: melhorias e alterações no design do site

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,27 @@
 import { Component, input, OnInit, signal } from '@angular/core';
 import { UserInfoComponent } from '../user-info/user-info.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [UserInfoComponent],
+  imports: [UserInfoComponent, MatMenuModule, MatIconModule, MatButtonModule],
 })
 export class HeaderComponent implements OnInit {
   showSlogan = input.required<boolean>();
   ballSize = signal('80px');
   textSize = signal('3rem');
+  borderSize = signal('3px');
   showUserInfo = input(false);
 
   ngOnInit() {
     if (!this.showSlogan()) {
       this.ballSize.set('60px');
       this.textSize.set('2rem');
+      this.borderSize.set('2.5px');
     }
   }
 }
